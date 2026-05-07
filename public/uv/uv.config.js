5588b6abf81dc8e68041b9ff839640405a50d136
pinapples232rl-del <pinapples232rl@gmail.com>
2026-05-06 20:13:17 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: "/uv/service/",
-    bare: "/b/",
+    bare: 'https://rho-vercel-app.vercel.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/uv/uv.handler.js",
